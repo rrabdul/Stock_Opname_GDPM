f0fa967384d9435e07cdb77c1b818a715236d359
rrandul <rofiqabdul983@gmail.com>
2025-08-03 12:10:56 +0700

    refactor: clean up Profile component and improve sidenav layout

--- resources/views/layouts/sidenav.blade.php
+++ resources/views/layouts/sidenav.blade.php
@@ -65,7 +65,7 @@
                 <!-- Tambahkan icon jika mau -->
             </span>
             <span class="sidebar-text">Data Barang</span>
-        </a>
+          </a>
         </li>
         <li class="nav-item">
           <span class="nav-link collapsed d-flex justify-content-between align-items-center" data-bs-toggle="collapse"
@@ -87,7 +87,7 @@
                   clip-rule="evenodd"></path>
               </svg></span>
           </span>
-            <div
+          <div
             class="multi-level collapse {{ Request::routeIs('transaction.in') || Request::routeIs('transaction.out') || Request::routeIs('transaction.return') ? 'show' : '' }}"
             role="list" id="submenu-components" aria-expanded="false">
             <ul class="flex-column nav">
@@ -107,62 +107,67 @@
                 </a>
                 </li>
             </ul>
-            </div>
-            </li>
-
-            <li class="nav-item">
-    <span class="nav-link collapsed d-flex justify-content-between align-items-center" data-bs-toggle="collapse"
-        data-bs-target="#submenu-stocktacking">
-        <span>
-            <span class="sidebar-icon">
-                <svg class="icon icon-xs me-2" fill="currentColor" viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg">
-                    <path d="M4 4a2 2 0 00-2 2v1h16V6a2 2 0 00-2-2H4z"></path>
-                    <path fill-rule="evenodd"
-                        d="M3 8h14v7a2 2 0 01-2 2H5a2 2 0 01-2-2V8zm5 3a1 1 0 011-1h2a1 1 0 110 2H9a1 1 0 01-1-1z"
-                        clip-rule="evenodd"></path>
-                </svg>
-            </span>
-            <span class="sidebar-text">Stock Tacking</span>
-        </span>
-        <span class="link-arrow">
-            <svg class="icon icon-sm" fill="currentColor" viewBox="0 0 20 20"
+          </div>
+        </li>
+        <li class="nav-item">
+          <span class="nav-link collapsed d-flex justify-content-between align-items-center" data-bs-toggle="collapse"
+              data-bs-target="#submenu-stocktacking">
+              <span>
+                  <span class="sidebar-icon">
+                      <svg class="icon icon-xs me-2" fill="currentColor" viewBox="0 0 20 20"
+                          xmlns="http://www.w3.org/2000/svg">
+                          <path d="M4 4a2 2 0 00-2 2v1h16V6a2 2 0 00-2-2H4z"></path>
+                          <path fill-rule="evenodd"
+                              d="M3 8h14v7a2 2 0 01-2 2H5a2 2 0 01-2-2V8zm5 3a1 1 0 011-1h2a1 1 0 110 2H9a1 1 0 01-1-1z"
+                              clip-rule="evenodd"></path>
+                      </svg>
+                  </span>
+                  <span class="sidebar-text">Stock Tacking</span>
+              </span>
+              <span class="link-arrow">
+                  <svg class="icon icon-sm" fill="currentColor" viewBox="0 0 20 20"
+                      xmlns="http://www.w3.org/2000/svg">
+                      <path fill-rule="evenodd"
+                          d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                          clip-rule="evenodd"></path>
+                  </svg>
+              </span>
+          </span>
+          <div class="multi-level collapse {{ Request::routeIs('stocktaking.create') || Request::routeIs('stocktaking.gdtp') || Request::routeIs('stocktaking.production') ? 'show' : '' }}"
+              role="list" id="submenu-stocktacking" aria-expanded="false">
+              <ul class="flex-column nav">
+                  <li class="nav-item {{ Request::routeIs('stocktaking.create') ? 'active' : '' }}">
+                      <a class="nav-link" href="{{ route('stocktaking.create') }}">
+                          <span class="sidebar-text">Create</span>
+                      </a>
+                  </li>
+                  <li class="nav-item {{ Request::routeIs('stocktaking.gdtp') ? 'active' : '' }}">
+                      <a class="nav-link" href="{{ route('stocktaking.gdtp') }}">
+                          <span class="sidebar-text">GDTP</span>
+                      </a>
+                  </li>
+                  <li class="nav-item {{ Request::routeIs('stocktaking.production') ? 'active' : '' }}">
+                      <a class="nav-link" href="{{ route('stocktaking.production') }}">
+                          <span class="sidebar-text">Production</span>
+                      </a>
+                  </li>
+              </ul>
+          </div>
+        </li>
+        <li class="nav-item">
+          <a href="" target="_blank"
+            class="nav-link d-flex align-items-center">
+            <span class="sidebar-icon"><svg class="icon icon-xs me-2" fill="currentColor" viewBox="0 0 20 20"
                 xmlns="http://www.w3.org/2000/svg">
                 <path fill-rule="evenodd"
-                    d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                    clip-rule="evenodd"></path>
-            </svg>
-        </span>
-    </span>
-
-    <div
-        class="multi-level collapse {{ Request::routeIs('stocktaking.create') || Request::routeIs('stocktaking.gdtp') || Request::routeIs('stocktaking.production') ? 'show' : '' }}"
-        role="list" id="submenu-stocktacking" aria-expanded="false">
-        <ul class="flex-column nav">
-
-            <li class="nav-item {{ Request::routeIs('stocktaking.create') ? 'active' : '' }}">
-                <a class="nav-link" href="{{ route('stocktaking.create') }}">
-                    <span class="sidebar-text">Create</span>
-                </a>
-            </li>
-
-            <li class="nav-item {{ Request::routeIs('stocktaking.gdtp') ? 'active' : '' }}">
-                <a class="nav-link" href="{{ route('stocktaking.gdtp') }}">
-                    <span class="sidebar-text">GDTP</span>
-                </a>
-            </li>
-
-            <li class="nav-item {{ Request::routeIs('stocktaking.production') ? 'active' : '' }}">
-                <a class="nav-link" href="{{ route('stocktaking.production') }}">
-                    <span class="sidebar-text">Production</span>
-                </a>
-            </li>
-
-        </ul>
-    </div>
-</li>
+                  d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
+                  clip-rule="evenodd"></path>
+              </svg></span>
+            <span class="sidebar-text">Profile</span>
+          </a>
+        </li>
 
                 </li>
-      <li role="separator" class="dropdown-divider mt-4 mb-3 border-gray-700"></li>
+    <li role="separator" class="dropdown-divider mt-4 mb-3 border-gray-700"></li>
   </div>
 </nav>
